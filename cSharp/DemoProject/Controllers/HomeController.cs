51c1ce1ae42e01182b8d0f0dd6e3dc34fbf3efd6
phil-krull <pkrull@codingdojo.com>
2018-07-06 12:18:51 -0500

    Adding cSharp dotnet mvc walkthrough

---
+++ cSharp/DemoProject/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DemoProject.Models;
+
+namespace DemoProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Phil cloneOne = new Phil()
+            {
+                Name = "Lihp"
+            };
+            Console.WriteLine($"Phil class name is: {cloneOne.Name}");
+            Console.WriteLine($"Phil class height is: {cloneOne.Height}");
+
+            return View(cloneOne);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+        public IActionResult Test()
+        {
+            ViewData["Message"] = "From the test method";
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ cSharp/DemoProject/Models/Phil.cs
@@ -0,0 +1,18 @@
+namespace DemoProject.Models
+{
+    public class Phil
+    {
+        public string Name {get;set;}
+        public int Height {get;set;} = 100;
+        // or
+        // private string Name;
+        // public string getName()
+        // {
+        //     return Name;
+        // }
+        // public void setName(string name)
+        // {
+        //     Name = name;
+        // }
+    }
+}
